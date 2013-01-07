6e44a066aaadbd97117073e15f62ef81140bd60f
Redsandro Media <redsandro@gmail.com>
2013-01-07 15:26:13 +0100

    Updated package RenameMaster

--- renamemaster/tools/chocolateyInstall.ps1
+++ renamemaster/tools/chocolateyInstall.ps1
@@ -1,7 +1,21 @@
-try { 
-	$packageName = 'RenameMaster'
-	$zipUrl = Get-FilenameFromRegex "http://www.joejoesoft.com/vcms/hot/userupload/8/files/rmv303.zip" '/cms/file.php\?f=userupload/8/files/rmv303.zip&amp;c=([\w\d]+)' 'http://www.joejoesoft.com/sim/$1/userupload/8/files/rmv303.zip'
+$name = 'RenameMaster'
+$pwd = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
 
+write-host ''
+write-host "!!!ATTENTION!!!"
+write-host ''
+write-host "This is weird."
+write-host "This package fails the first time, but works the second time."
+write-host "Any thoughts? Let me know."
+write-host ''
+
+try { 
+	# Redsandro - I will just include the required modules with the package as long as they are not merged with Chocolatey yet
+	Import-Module "$($pwd)\Get-FilenameFromRegex.ps1"
+	Import-Module "$($pwd)\Install-ChocolateyPinnedItem.ps1"
+	# Why does an import failure on this module not throw an error?
+	
+	$url = Get-FilenameFromRegex "http://www.joejoesoft.com/vcms/hot/userupload/8/files/rmv303.zip" '/cms/file.php\?f=userupload/8/files/rmv303.zip&amp;c=([\w\d]+)' 'http://www.joejoesoft.com/sim/$1/userupload/8/files/rmv303.zip'
 
 	# Calculate $binRoot, which imo should always be set in $env:chocolatey_bin_root as a full path (not relative)
 	if($env:chocolatey_bin_root -eq $null) {
@@ -16,16 +30,17 @@ try {
 		$binRoot = $env:chocolatey_bin_root
 	}
 
-	Install-ChocolateyZipPackage $packageName "$zipUrl" "$binRoot"
+	Install-ChocolateyZipPackage $name "$url" "$binRoot"
 	
-	# Copy "$binRoot\renamemaster.exe" shortcut to start menu
-	
+	# Copy 'Rename Master' shortcut to start menu
+	Install-ChocolateyPinnedItem "$binRoot\RenameMaster.exe"
+
+	# Add 'Rename Master... to context menu for directories'
 	Install-ChocolateyExplorerMenuItem 'openRenameMaster' 'Rename Master...' "$binRoot\RenameMaster.exe" directory
 
-	Write-ChocolateySuccess $packageName
+	Write-ChocolateySuccess $name
 } catch {
-	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+	Write-ChocolateyFailure $name "$($_.Exception.Message)"
 	throw 
 }
 
-#mklink \bin\target.exe \original\file.exe
