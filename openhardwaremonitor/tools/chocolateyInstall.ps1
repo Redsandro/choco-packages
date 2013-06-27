ca6d8960977ae030f1b44ea3e00ed7c155b9b88f
Redsandro Media <redsandro@gmail.com>
2013-06-27 17:42:11 +0200

    Added OpenHardwareMonitor package

---
+++ openhardwaremonitor/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+$name = 'OpenHardwareMonitor'
+$pwd = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://openhardwaremonitor.org/files/openhardwaremonitor-v0.5.1-beta.zip'
+
+
+try { 
+	# Redsandro - I will just include the required modules with the package as long as they are not merged with mainline Chocolatey yet
+	Import-Module "$($pwd)\Install-ChocolateyPinnedItem.ps1"
+	Import-Module "$($pwd)\Get-BinRoot.ps1"
+		
+	$binRoot = Get-BinRoot
+
+	# Extract archive to binRoot
+	Install-ChocolateyZipPackage $name "$url" "$binRoot"
+	
+	# Pin 'OpenHardwareMonitor' to Start
+	Install-ChocolateyPinnedItem "$binRoot\OpenHardwareMonitor\OpenHardwareMonitor.exe"
+
+	Write-ChocolateySuccess $name
+} catch {
+	Write-ChocolateyFailure $name "$($_.Exception.Message)"
+	throw 
+}
+
