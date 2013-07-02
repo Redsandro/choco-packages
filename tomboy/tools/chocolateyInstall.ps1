70038a0bfc7f0565b9d006609de8a3041ab16b18
Redsandro Media <redsandro@gmail.com>
2013-07-02 17:38:16 +0200

    New package Partition Master Free

--- tomboy/tools/chocolateyInstall.ps1
+++ tomboy/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 ﻿$packageName = 'tomboy'
 $installerType = 'msi' 
-$url = 'http://download.gnome.org/binaries/win32/tomboy/1.10/Tomboy-1.10.2.msi'
+# $url = 'http://download.gnome.org/binaries/win32/tomboy/1.14/Tomboy-1.14.0.msi'
+$url = 'http://ftp.gnome.org/pub/GNOME/binaries/win32/tomboy/1.15/Tomboy-1.15.1.msi'
 $url64 = $url # 64bit URL here or just use the same as $url
 $silentArgs = '/quiet'
 $validExitCodes = @(0,3010) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
