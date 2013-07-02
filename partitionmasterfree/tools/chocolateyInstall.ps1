ac54464820dd0986e35444350988da058fb621a0
Redsandro Media <redsandro@gmail.com>
2013-07-02 17:38:16 +0200

    New package Partition Master Free

---
+++ partitionmasterfree/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+$packageName = 'PartitionMasterFree'
+$installerType = 'exe' 
+$url = 'http://download.easeus.com/free/epm.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
