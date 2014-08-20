2bced82ed3f993c00d2e3d94b37cc19c4eaae3f4
Redsandro <redsandro@gmail.com>
2014-08-20 18:26:10 +0200

    Patch-My-PC added. %0@! pin to Start not working. Someone PLEASE FIX.

--- patch-my-pc/tools/chocolateyInstall.ps1
+++ patch-my-pc/tools/chocolateyInstall.ps1
@@ -1,17 +1,40 @@
 $name = 'Patch my PC'
 $id = 'patch-my-pc'
 $url = 'http://patchmypc.net/PatchMyPC.exe'
+$pwd = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
+
+# Combatibility - This function has not been merged
+if (!(Get-Command Install-ChocolateyPinnedItem -errorAction SilentlyContinue)) {
+	Import-Module "$($pwd)\Install-ChocolateyPinnedItem.ps1"
+}
 
 try {
-    Write-Debug "Downloading $name from '$url'";
 
+	$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     $chocTempDir = Join-Path $env:TEMP "chocolatey"
     $tempDir = Join-Path $chocTempDir "$id"
 
-    if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-    $file = Join-Path $tempDir "$($id)Install.$fileType"
+	# Calculate $binRoot, which should always be set in $env:ChocolateyBinRoot as a full path (not relative)
+	$binRoot = Get-BinRoot;
 
-    Get-ChocolateyWebFile $id $file $url
+    #if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
+    #$file = Join-Path $tempDir "$($id)Install.$fileType"
+    $file = "$binRoot\PatchMyPC.exe"
+    $tempFile = "$pwd\PatchMyPC.exe"
+
+    Write-Host "Did someone change the binRoot?";
+    Write-Host "Old: $binRoot";
+    Write-Host "New: $nugetExePath";
+
+    Get-ChocolateyWebFile $id "$tempFile" "$url"
+
+	# Copy shortcut to start menu
+#	Install-ChocolateyPinnedItem "$file"
+#	Install-ChocolateyPinnedItem "$tempFile"
+
+	$meh = Join-Path $nugetExePath "PatchMyPC.exe"
+	
+	Install-ChocolateyPinnedItem $meh
 
     Write-ChocolateySuccess $name
 } catch {
