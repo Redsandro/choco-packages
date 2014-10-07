cf077536ff9417c398753616ed9b0fda6cce73ea
Redsandro <redsandro@gmail.com>
2014-10-07 17:56:37 +0200

    Converted to autopackage

--- autopackages/keepassx2-disabled/tools/chocolateyInstall.ps1
+++ autopackages/keepassx2-disabled/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$packageName	= 'KeePassX'
+$packageName	= 'KeePassX2-alpha'
 $installerType	= 'EXE'
 $url			= '{{DownloadUrl}}'
 $silentArgs		= '/VERYSILENT'
@@ -13,9 +13,9 @@ Install-ChocolateyZipPackage "$packageName" "$url" "$pwd"
 
 $appData = [environment]::GetFolderPath([environment+specialfolder]::ApplicationData)
 $destPath = Join-Path "$appData" "Microsoft\Windows\Start Menu\Programs\"
-$destLink = Join-Path "$destPath" "KeePassX.lnk"
+$destLink = Join-Path "$destPath" "KeePassX 2.0 alpha.lnk"
 
 $shell = New-Object -comObject WScript.Shell
 $shortcut = $shell.CreateShortcut($destLink)
-$shortcut.TargetPath = Join-Path "$pwd" "KeePassX\KeePassX.exe"
+$shortcut.TargetPath = Join-Path "$pwd" "KeePassX-1.9.85\KeePassX.exe"
 $shortcut.Save()
--- partitionmasterfree/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName = 'PartitionMasterFree'
-$installerType = 'exe' 
-$url = 'http://download.easeus.com/free/epm.exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
-
-
