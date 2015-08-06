d5a0b7e128ddcfd615ec8e108ce648409b37ab5f
Christian Foellmann <foellmann@foe-services.de>
2015-08-06 14:04:05 +0200

    update ultradefrag to 6.1.0

--- ultradefrag/tools/chocolateyInstall.ps1
+++ ultradefrag/tools/chocolateyInstall.ps1
@@ -1,12 +1,8 @@
 $packageName = 'ultradefrag'
 $installerType = 'exe' 
-#$url = 'http://switch.dl.sourceforge.net/project/ultradefrag/stable-release/5.1.2/ultradefrag-6.0.2.bin.i386.exe'
-#$url = 'http://garr.dl.sourceforge.net/project/ultradefrag/stable-release/6.0.2/ultradefrag-6.0.2.bin.i386.exe'
-$url = 'http://garr.dl.sourceforge.net/project/ultradefrag/latest-release-candidates/7.0.0%20beta1/ultradefrag-7.0.0-beta1.bin.i386.exe'
-#$url64 = 'http://freefr.dl.sourceforge.net/project/ultradefrag/stable-release/5.1.2/ultradefrag-5.1.2.bin.amd64.exe'
-#$url64 = 'http://heanet.dl.sourceforge.net/project/ultradefrag/stable-release/6.0.2/ultradefrag-6.0.2.bin.amd64.exe'
-$url64 = 'http://heanet.dl.sourceforge.net/project/ultradefrag/latest-release-candidates/7.0.0%20beta1/ultradefrag-7.0.0-beta1.bin.amd64.exe'
 $silentArgs = '/S /FULL=1'
+$url = 'https://sourceforge.net/projects/ultradefrag/files/stable-release/6.1.0/ultradefrag-6.1.0.bin.i386.exe/download'
+$url64 = 'https://sourceforge.net/projects/ultradefrag/files/stable-release/6.1.0/ultradefrag-6.1.0.bin.amd64.exe/download'
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
 
