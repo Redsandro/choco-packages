e2600dcfb98c3a5c42e39cd19c521719ffae676c
Sander Steenhuis <redsandro@gmail.com>
2016-06-08 12:16:40 +0200

    Moved packages to chocolatey-coreteampackages

--- autopackages/keepassx/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$unfile = "KeePass Password Safe/unins000.exe"
-
-if (Test-Path "${Env:ProgramFiles(x86)}\$unfile") {
-    $unpath = "${Env:ProgramFiles(x86)}\$unfile"
-}
-else {
-    $unpath = "${Env:ProgramFiles}\$unfile"
-}
-Uninstall-ChocolateyPackage 'KeePass Classic' 'exe' '/VERYSILENT' "$unpath"
--- freeotfe/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-$packageName = 'freeotfe'
-$installerType = 'exe' 
-$url = 'http://www.freeotfe.org/downloads/FreeOTFE_5_21.exe'
-$url64 = $url # 64bit URL here or just use the same as $url
-$silentArgs = '/S'
-
-# main helpers - these have error handling tucked into them already
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
