eb557acd125108795fe35de8cd9f16836e8c99c6
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-14 01:42:07 +0200

    (reflect-free) created ahk installer insetad of passive

--- automatic/reflect-free/tools/chocolateyInstall.ps1
+++ automatic/reflect-free/tools/chocolateyInstall.ps1
@@ -1,25 +1,19 @@
 ﻿$toolsDir  = Split-Path $MyInvocation.MyCommand.Definition
 
 $url  = 'http://updates.macrium.com/reflect/v6/ReflectDL.exe'
-$fileName  = $url -split '/' | select -Last 1
-$dwnAgentPath = "$(Get-ToolsLocation)\$fileName"
-Get-WebFile $url $dwnAgentPath
+$agentfileName  = $url -split '/' | select -Last 1
+$downloadDir = "$(Get-ToolsLocation)\reflect-free"
+rm $downloadDir -Recurse -ea 0
+
+Get-WebFile $url "$downloadDir\$agentfileName"
 
 Write-Host "Running Macrium download agent via Autohotkey"
+Autohotkey.exe $toolsDir\download.ahk "$downloadDir\$agentfileName" $downloadDir
+$installer = gi $downloadDir\Macrium\*.exe -ea 0
+if (!$installer) { Write-Host "Can't automate Macrium download agent, please run manually $downloadDir" }
 
-rm $Env:TEMP\MacriumInstall\Macrium\* -ea 0
-Autohotkey.exe $toolsDir\install.ahk $dwnAgentPath
-$installer = gi $Env:TEMP\MacriumInstall\Macrium\*.exe -ea 0
-if (!$installer) { Write-Host "Can't automated Macrium install, please run manually $dwnAgentPath" }
+Write-Host "Running Macrium installer via Autohotkey: $installer"
+Autohotkey.exe $toolsDir\install.ahk $installer
 
-$packageArgs = @{
-  packageName    = 'reflect-free'
-  fileType       = 'exe'
-  file           = $installer
-  silentArgs     = '/passive'
-  validExitCodes = @(0)
-  sowftwareName  = 'Macrium Reflect*'
-}
-Install-ChocolateyInstallPackage @packageArgs
-# rm $Env:TEMP\MacriumInstall\Macrium\* -ea 0
-Write-Host "The installer might still be at $installer"
+Write-Host "Installation completed"
+Write-Host "Downloaded files are left in: $downloadDir"
