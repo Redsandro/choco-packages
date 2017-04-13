88c42d5d2992b83a889a75528cd3bbaafb4097eb
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-14 00:18:54 +0200

    (reflect-free) fixed AU updater

--- automatic/reflect-free/update.ps1
+++ automatic/reflect-free/update.ps1
@@ -11,10 +11,10 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $releases       = 'http://www.macrium.com/reflectfree.aspx'
   $download_page  = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $version        = $download_page.Content -split '\n' | ? {$_ -match 'Current Version:'} | % {$_ -replace '.+Current Version:.+;([0-9\.]+).+', '$1'}
+  $version        = $download_page.Content -split '\n' | ? {$_ -match 'Current Version:'} | % {$_ -replace '.+Current Version: ([0-9\.]+).+', '$1'}
 
   @{
-    Version  = "$version"
+    Version  = $version
     URL32    = 'http://updates.macrium.com/reflect/v6/ReflectDL.exe'
   }
 }
